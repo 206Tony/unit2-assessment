6293d05347a73d4bc817cce031b3f24ae4f7d626
Anthony Zardis <anthonyzardis@gmail.com>
2019-07-09 16:17:03 -0700

    Working

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
-
+const db = require('./models');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -8,10 +8,27 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.static('static'));
 app.use(methodOverride('_method'));
 
-// WRITE YOUR ROUTES HERE /////////////////////
+app.get('/', function(req, res) {
+  db.widget.findAll().then(function(widgets) {
+    res.render('index', {widgets});
+  });
+});
 
+app.post('/', function(req, res) {
+  db.widget.create({
+    description: req.body.description,
+    quantity: parseInt(req.body.quantity)
+  }).then(function(widgets) {
+    res.redirect('/');
+  });
+});
 
-
-// YOUR ROUTES ABOVE THIS COMMENT /////////////
+app.delete('/:id', function(req, res) {
+  db.widget.destroy({
+    where: {id: parseInt(req.params.id)}
+  }).then(function(data) {  
+      res.redirect("/");
+  });
+});
 
 app.listen(3000);
